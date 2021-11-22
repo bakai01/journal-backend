4f530716ea1600f95388adc8f7e4f3296f3ad1ee
bakai <thedesport20@gmail.com>
2021-11-22 12:18:23 +0600

    improvements for create-user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -17,7 +17,18 @@ export class UsersService {
     return user;
   };
 
+  private async getUserByEmail(email: string): Promise<CreateUserDto> | undefined {
+    const user = await this.usersRepository.findOne({ email });
+    return user;
+  };
+
   async createUser(dto: CreateUserDto): Promise<CreateUserDto> {
+    const userExist = await this.getUserByEmail(dto.email);
+
+    if (userExist) {
+      throw new HttpException('Пользователь с такой почтой уже существует', HttpStatus.BAD_REQUEST);
+    }
+
     const user = await this.usersRepository.save(dto);
     return user;
   };
